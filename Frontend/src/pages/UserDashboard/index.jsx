24766c63547c486173adefe858f551a6248de165
Sumit <sumitghimire018@gmail.com>
2023-11-27 16:12:50 +0545

    a

--- Frontend/src/pages/UserDashboard/index.jsx
+++ Frontend/src/pages/UserDashboard/index.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import NavBar from "../../components/Navbar";
+import Chat from "../../components/Chat";
 
 const UserDashboard = () => {
   return (
     <>
-      <NavBar />
+      <Chat />
+      {/* <NavBar />
       <div className="mt-[15vh] ml-10 bg-red-500 inline-block">
         <Link to="/user/form">Create Profile</Link>
-      </div>
+      </div> */}
     </>
   );
 };
--- Frontend/src/pages/UserForm/index.jsx
+++ Frontend/src/pages/UserForm/index.jsx
@@ -139,3 +139,5 @@ const Form = () => {
 };
 
 export default Form;
+
+
